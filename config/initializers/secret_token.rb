f8dd58e21cc046b24710915ac68dbcae4e15214d
JonLz <jon.lazar@gmail.com>
2012-10-27 13:44:04 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wordhacker::Application.config.secret_token = 'd9b164dfb0bcf8abc8f5d51b4bf392167c57671b208029bd632fbf8239298360efff188c5b427d167bda92159fe24e86b87a2ef6f76b0e011f6e406453aa5a38'
